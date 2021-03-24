569791ec46335d8b691747823b25284bc15a9d46
Hannes <h@nnes.dev>
2021-03-24 11:31:13 +0000

    Move example to tests

--- src/TernaryPlots.jl
+++ src/TernaryPlots.jl
@@ -4,6 +4,8 @@ using Plots
 using LinearAlgebra
 using StaticArrays
 
+export ternary_plot, tern2cart
+
 function tern2cart(a, b, c)
     return (1 / 2 * (2b + c) / (a + b + c), √3 / 2 * (c / (a + b + c)))
 end
@@ -184,29 +186,5 @@ function ternary_plot(;
 
     return p
 end
-
-# p = ternary_plot(
-#     title="Distribution of A, B, and C in system",
-#     labels = (A = "Cheaters (%)", B = "Sitters (%)", C = "Idenfitiers (%)"),
-# )
-
-## Plotting Points
-# npoints = 10
-# points = [Vector{Float64}() for i in 1:npoints]
-# accepted_points = 0
-
-# while accepted_points < npoints
-#     point = rand(SVector{2, Float64})
-#     if sum(point) <= 1
-#         points[accepted_points+1] = vcat(point, 1 - sum(point))
-#         accepted_points += 1
-#     end
-# end
-
-# points_to_plot = tern2cart.(points)
-# x_to_plot = first.(points_to_plot)
-# y_to_plot = last.(points_to_plot)
-
-# Plots.plot!(p, x_to_plot, y_to_plot, seriestype = :scatter)
 
 end # module
