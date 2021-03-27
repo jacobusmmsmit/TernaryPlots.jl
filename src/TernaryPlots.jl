25f7f996e3c7b0a13678ef5457f0f6c7df55d87c
jacobusmmsmit <57063134+jacobusmmsmit@users.noreply.github.com>
2021-03-27 19:31:51 +0000

    Export ternary_plot!

--- src/TernaryPlots.jl
+++ src/TernaryPlots.jl
@@ -3,7 +3,7 @@ module TernaryPlots
 using Plots
 using LinearAlgebra
 
-export ternary_plot, tern2cart, cart2tern
+export ternary_plot, ternary_plot!, tern2cart, cart2tern
 export ternary_heatmap, ternary_heatmap!
 export ternary_contour, ternary_contour!
 
