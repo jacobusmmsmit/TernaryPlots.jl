7a5beaa6758dc2318895a99b20bd000a9b41cccd
Martin Smit <57063134+jacobusmmsmit@users.noreply.github.com>
2021-09-02 15:53:19 +0100

    Add documentation

--- src/user_helpers.jl
+++ src/user_helpers.jl
@@ -1,6 +1,15 @@
+"""
+    tern2cart(a, b, c)
+Takes a point in the ternary coordinate system and returns the cartesian co-ordinate of this point.
+The ternary axes are all between 0 and 1 and the cartesian co-ordinates form an equilateral triangle at (0, 0), (1, 0), (1/2, √3/2)
+"""
 tern2cart(a, b, c) = (1 / 2 * (2b + c) / (a + b + c), √3 / 2 * (c / (a + b + c)))
 tern2cart(vec) = tern2cart(vec[1], vec[2], vec[3])
 
+"""
+    cart2tern(x, y)
+Takes a point in the cartesian coordinate system and returns the ternary co-ordinate of this point.
+"""
 function cart2tern(x, y)
     c = (2 * y) / √3
     b = x - c / 2
