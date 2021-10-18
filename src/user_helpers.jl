132d2231d58b385e60bb12ec7e98a6adb2261852
Hannes <h@nnes.dev>
2021-10-18 23:37:27 +0100

    Fix formatting

--- src/user_helpers.jl
+++ src/user_helpers.jl
@@ -1,5 +1,6 @@
 """
     tern2cart(a, b, c)
+
 Takes a point in the ternary coordinate system and returns the cartesian co-ordinate of this point.
 The ternary axes are all between 0 and 1 and the cartesian co-ordinates form an equilateral triangle at (0, 0), (1, 0), (1/2, √3/2)
 """
@@ -8,6 +9,7 @@ tern2cart(vec) = tern2cart(vec[1], vec[2], vec[3])
 
 """
     cart2tern(x, y)
+
 Takes a point in the cartesian coordinate system and returns the ternary co-ordinate of this point.
 """
 function cart2tern(x, y)
